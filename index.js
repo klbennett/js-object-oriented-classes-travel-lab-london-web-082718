69ad00c87f96b94894f21836040a829dd00a6779
Kay B <kaylouisebennett@gmail.com>
2018-10-13 08:25:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ class Driver {
   }
 
   yearsExperienceFromBeginningOf(endDate) {
-    let myDate = new Date(new Date().getFullYear(), 0, 1);
+    let myDate = new Date().getFullYear(), 0, 1);
     return this.startDate.getFullYear() - myDate;
   }
 }
